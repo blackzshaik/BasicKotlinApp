3ab3cb912f2c0596ffa74d8cb975a2f74eec649a
Blackz Shaik <blackzshaik@gmail.com>
2019-09-29 00:35:31 +0530

    set edittext and button, set button onclicklistener, toast message with text input form edittext

--- app/src/main/java/com/madtutorials/basickotlinapp/MainActivity.kt
+++ app/src/main/java/com/madtutorials/basickotlinapp/MainActivity.kt
@@ -2,6 +2,8 @@ package com.madtutorials.basickotlinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -10,13 +12,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val message = "Hello World From Kotlin"
+        val message = "Welcome From Kotlin"
         val duration = Toast.LENGTH_LONG
         val toast = Toast.makeText(applicationContext,message,duration)
 
+    /* Remove this comment to see both the toasts
         toast.show()
-
+    */
         //short_hand
-        Toast.makeText(applicationContext,"Hello Form Kotlin",Toast.LENGTH_LONG).show()
+        Toast.makeText(applicationContext,"Welcome Form Kotlin",Toast.LENGTH_LONG).show()
+
+        val editext = findViewById<EditText>(R.id.editText) //get edittext using id
+        val button = findViewById<Button>(R.id.toastButton) //get button using id
+
+        button.setOnClickListener { //setting listener function to button , using lambda expression , called when button clicked
+            val message2 = editext.text.toString() //get input from edittext and convert to text
+
+            Toast.makeText(applicationContext, message2, Toast.LENGTH_SHORT).show() //display toast with input
+        }
+
     }
 }
