17a5612588c538b284e7017ee289fa95af6db976
Blackz Shaik <blackzshaik@gmail.com>
2019-09-28 23:05:06 +0530

    added toast message code

---
+++ app/src/main/java/com/madtutorials/basickotlinapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.madtutorials.basickotlinapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val message = "Hello World From Kotlin"
+        val duration = Toast.LENGTH_LONG
+        val toast = Toast.makeText(applicationContext,message,duration)
+
+        toast.show()
+    }
+}
